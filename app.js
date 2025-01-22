f4bcfd24290b34f81a1b0e66acbc0e0fda9ea694
Atonalsv <otto_alejandro_marroquin@yahoo.com>
2025-01-21 23:15:02 -0600

    actualizacion (agregar amifo a lista)

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 //variables
-let listaDeNombres = []
+let listaAmigos = []
+let resultado = (0)
+let nombreDeAmigo = (0)
+
+function asignarTextoElemento(elemento,texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHtml = texto;
+    return;
+}
+
+asignarTextoElemento('h1','Amigo secreto!2');
+asignarTextoElemento('h2','Digite el nombre de sus amigos!2');
 
 
+function agregarAmigo() {
+    //agregar nombre de amigo a la lista
+    let nombreDeAmigo = document.getElementById('amigo').value;
+    listaAmigos.push(nombreDeAmigo);
+    console.log(listaAmigos);
+    return
+}
+
+function sortearAmigo() {
+
+}
 
