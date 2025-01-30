8a594bd1d65c93f051291a2363db04c73921289c
Atonalsv <otto_alejandro_marroquin@yahoo.com>
2025-01-29 23:58:14 -0600

    Actualizcion de codigo

--- app.js
+++ app.js
@@ -7,34 +7,45 @@ let nombreDeAmigo = (0)
 let amigoSorteado = (0)
 let nombreSeleccionado = (0)
 
+
 function asignarTextoElemento(elemento,texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHtml = texto;
-    return;
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
 }
 
 asignarTextoElemento('h1','Amigo secreto!2');
 asignarTextoElemento('h2','Digite el nombre de sus amigos!2');
 
 
+
 function agregarAmigo() {
-    //agregar nombre de amigo a la lista
+    //Verifica si es valido con if y lo agrega a la lista
     let nombreDeAmigo = document.getElementById('amigo').value;
-    listaAmigos.push(nombreDeAmigo);
+    if (nombreDeAmigo == '') {
+        alert('Por favor ingrese un nombre de amigo valido.')
+    } else {
+        listaAmigos.push(nombreDeAmigo);
+    }
+    asignarTextoElemento("listaAmigos",`${listaAmigos}`);
     console.log(listaAmigos);
     return
 }
+
 
 function sortearAmigo() {
     //Usando la list de amigos, toma uno para mostrar como nombre de amigo sorteado
     let amigoSorteado = Math.floor(Math.random()*listaAmigos.length);
     let nombreSeleccionado = listaAmigos[amigoSorteado];
+    //Muestra amigo sorteado en pantalla y consoleLog
+    asignarTextoElemento("resultado",`${amigoSorteado}`);
     console.log(amigoSorteado);
     console.log(nombreSeleccionado);
     return
 
 }
-
-
-
+/*
+Falta hacer que lista de amigos desaparezca cuando seleccione el boton sortear
+Falta que lista de amigos se muestre en vertical
+Falta que formato de lista de amigos muestre en rojo
+*/
 
