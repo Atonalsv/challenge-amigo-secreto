1185cb79ec40a88b8daaba22c883fc7f6cd305e2
Atonalsv <otto_alejandro_marroquin@yahoo.com>
2025-01-31 00:28:08 -0600

    Actualizacion proyecto (vaciar elemento)

--- app.js
+++ app.js
@@ -2,31 +2,22 @@
 
 //variables
 let listaAmigos = []
+let nombresSorteados = []
 let resultado = (0)
 let nombreDeAmigo = (0)
 let amigoSorteado = (0)
 let nombreSeleccionado = (0)
 
-
-function asignarTextoElemento(elemento,texto) {
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerHTML = texto;
-}
-
-asignarTextoElemento('h1','Amigo secreto!2');
-asignarTextoElemento('h2','Digite el nombre de sus amigos!2');
-
-
-
+//Agrega nombre de amigo a la lista verificando si el imput es valido
 function agregarAmigo() {
-    //Verifica si es valido con if y lo agrega a la lista
     let nombreDeAmigo = document.getElementById('amigo').value;
     if (nombreDeAmigo == '') {
         alert('Por favor ingrese un nombre de amigo valido.')
     } else {
         listaAmigos.push(nombreDeAmigo);
     }
-    asignarTextoElemento("listaAmigos",`${listaAmigos}`);
+    asignarTextoElemento('listaAmigos',`${listaAmigos}`);
+    vaciarContenido('amigo');
     console.log(listaAmigos);
     return
 }
@@ -37,12 +28,22 @@ function sortearAmigo() {
     let amigoSorteado = Math.floor(Math.random()*listaAmigos.length);
     let nombreSeleccionado = listaAmigos[amigoSorteado];
     //Muestra amigo sorteado en pantalla y consoleLog
-    asignarTextoElemento("resultado",`${amigoSorteado}`);
+    asignarTextoElemento("resultado",`${nombreSeleccionado}`);
     console.log(amigoSorteado);
     console.log(nombreSeleccionado);
-    return
-
 }
+
+
+function asignarTextoElemento(elemento,texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function vaciarContenido(elemento) {
+    document.getElementById(elemento).value = '';
+}
+
+
 /*
 Falta hacer que lista de amigos desaparezca cuando seleccione el boton sortear
 Falta que lista de amigos se muestre en vertical
